720b185c054e802e9d74344b8d7db8324e3c13ff
minhqdao <hello@minhdao.de>
2024-09-30 18:32:54 +0700

    Add integration tests for int16 and uint32

--- test/integration_test/integration_test.dart
+++ test/integration_test/integration_test.dart
@@ -29,8 +29,42 @@ void main() {
       File(npyFilename).deleteSync();
       expect(result.exitCode, 0, reason: result.stderr.toString());
     });
-    test('3d list of int16', () {});
-    test('2d list of uint32, big endian', () {});
+    test('3d list of int16', () async {
+      const npyFilename = '${baseDir}save_int_test.npy';
+      const pythonScript = '${baseDir}load_int_test.py';
+      await saveList(
+        npyFilename,
+        [
+          [
+            [1, 2, 3],
+            [-4, 5, 6],
+          ],
+          [
+            [-32768, 0, 9],
+            [10, 11, 32767],
+          ]
+        ],
+        dtype: NpyDType.int16(),
+      );
+      final result = await Process.run('python', [pythonScript, npyFilename]);
+      File(npyFilename).deleteSync();
+      expect(result.exitCode, 0, reason: result.stderr.toString());
+    });
+    test('2d list of uint32, big endian', () async {
+      const npyFilename = '${baseDir}save_uint_test.npy';
+      const pythonScript = '${baseDir}load_uint_test.py';
+      await saveList(
+        npyFilename,
+        [
+          [1, 2, 0],
+          [4294967295, 5, 6],
+        ],
+        dtype: NpyDType.uint32(endian: NpyEndian.big),
+      );
+      final result = await Process.run('python', [pythonScript, npyFilename]);
+      File(npyFilename).deleteSync();
+      expect(result.exitCode, 0, reason: result.stderr.toString());
+    });
   });
   group('Load:', () {});
 }
